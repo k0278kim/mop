e25078425cbc3d226d7890a0fa381f9f571d40d4
k0278kim <k0278kim@gmail.com>
2026-04-28 13:58:46 +0900

    예제 6-1

--- app/src/main/java/com/example/assignment/MainActivity.kt
+++ app/src/main/java/com/example/assignment/MainActivity.kt
@@ -1,20 +1,100 @@
 package com.example.assignment
 
+import android.graphics.Color
 import android.os.Bundle
+import android.os.SystemClock
+import android.view.View
+import android.widget.Button
+import android.widget.CalendarView
+import android.widget.Chronometer
+import android.widget.RadioButton
+import android.widget.TextView
+import android.widget.TimePicker
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var chrono: Chronometer
+    lateinit var btnStart: Button
+    lateinit var btnEnd: Button
+    lateinit var rdoCal: RadioButton
+    lateinit var rdoTime: RadioButton
+    lateinit var calView: CalendarView
+    lateinit var tPicker: TimePicker
+    lateinit var tvYear: TextView
+    lateinit var tvMonth: TextView
+    lateinit var tvDay: TextView
+    lateinit var tvHour: TextView
+    lateinit var tvMinute: TextView
+
+    var selectYear: Int = 0
+    var selectMonth: Int = 0
+    var selectDay: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         supportActionBar?.setDisplayShowHomeEnabled(true)
         supportActionBar?.setIcon(R.drawable.ic_launcher)
         title = "202235245 김태윤 8주차"
-
         setContentView(R.layout.activity_main)
+
+        btnStart = findViewById<Button>(R.id.btnStart)
+        btnEnd = findViewById<Button>(R.id.btnEnd)
+
+        chrono = findViewById<Chronometer>(R.id.chronometer1)
+
+        rdoCal = findViewById<RadioButton>(R.id.rdoCal)
+        rdoTime = findViewById<RadioButton>(R.id.rdoTime)
+
+        tPicker = findViewById<TimePicker>(R.id.timePicker1)
+        calView = findViewById<CalendarView>(R.id.calendarView1)
+
+        tvYear = findViewById<TextView>(R.id.tvYear)
+        tvMonth = findViewById<TextView>(R.id.tvMonth)
+        tvDay = findViewById<TextView>(R.id.tvDay)
+        tvHour = findViewById<TextView>(R.id.tvHour)
+        tvMinute = findViewById<TextView>(R.id.tvMinute)
+
+        tPicker.visibility = View.INVISIBLE
+        calView.visibility = View.INVISIBLE
+
+        rdoCal.setOnClickListener {
+            tPicker.visibility = View.INVISIBLE
+            calView.visibility = View.VISIBLE
+        }
+
+        rdoTime.setOnClickListener {
+            tPicker.visibility = View.VISIBLE
+            calView.visibility = View.INVISIBLE
+        }
+
+        btnStart.setOnClickListener {
+            chrono.base = SystemClock.elapsedRealtime()
+            chrono.start()
+            chrono.setTextColor(Color.RED)
+        }
+
+        btnEnd.setOnClickListener {
+            chrono.stop()
+            chrono.setTextColor(Color.BLUE)
+
+            tvYear.text = Integer.toString(selectYear)
+            tvMonth.text = Integer.toString(selectMonth)
+            tvDay.text = Integer.toString(selectDay)
+
+            tvHour.text = Integer.toString(tPicker.hour)
+            tvMinute.text = Integer.toString(tPicker.minute)
+        }
+
+        calView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            selectYear = year
+            selectMonth = month + 1
+            selectDay = dayOfMonth
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
