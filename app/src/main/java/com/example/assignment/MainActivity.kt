46af0e4c58ca6d5a279ed845165b59fff91fd108
k0278kim <k0278kim@gmail.com>
2026-04-28 14:26:31 +0900

    예제 6-11

--- app/src/main/java/com/example/assignment/MainActivity.kt
+++ app/src/main/java/com/example/assignment/MainActivity.kt
@@ -1,35 +1,15 @@
 package com.example.assignment
 
-import android.graphics.Color
 import android.os.Bundle
-import android.os.SystemClock
-import android.view.View
-import android.widget.Button
-import android.widget.Chronometer
-import android.widget.DatePicker
-import android.widget.RadioButton
-import android.widget.TextView
-import android.widget.TimePicker
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.MultiAutoCompleteTextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    lateinit var chrono: Chronometer
-    lateinit var rdoCal: RadioButton
-    lateinit var rdoTime: RadioButton
-    lateinit var calView: DatePicker
-    lateinit var tPicker: TimePicker
-    lateinit var tvYear: TextView
-    lateinit var tvMonth: TextView
-    lateinit var tvDay: TextView
-    lateinit var tvHour: TextView
-    lateinit var tvMinute: TextView
-
-    var selectYear: Int = 0
-    var selectMonth: Int = 0
-    var selectDay: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,67 +19,16 @@ class MainActivity : AppCompatActivity() {
         title = "202235245 김태윤 8주차"
         setContentView(R.layout.activity_main)
 
-        chrono = findViewById<Chronometer>(R.id.chronometer1)
+        var items = arrayOf("CSI-뉴욕", "CSI-라스베가스", "CSI-마이애미", "Friends", "Fringe", "Lost")
 
-        rdoCal = findViewById<RadioButton>(R.id.rdoCal)
-        rdoTime = findViewById<RadioButton>(R.id.rdoTime)
+        var auto = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView1)
+        var adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, items)
+        auto.setAdapter(adapter)
 
-        tPicker = findViewById<TimePicker>(R.id.timePicker1)
-        calView = findViewById<DatePicker>(R.id.calendarView1)
-
-        tvYear = findViewById<TextView>(R.id.tvYear)
-        tvMonth = findViewById<TextView>(R.id.tvMonth)
-        tvDay = findViewById<TextView>(R.id.tvDay)
-        tvHour = findViewById<TextView>(R.id.tvHour)
-        tvMinute = findViewById<TextView>(R.id.tvMinute)
-
-        tPicker.visibility = View.INVISIBLE
-        calView.visibility = View.INVISIBLE
-        rdoCal.visibility = View.INVISIBLE
-        rdoTime.visibility = View.INVISIBLE
-
-        rdoCal.setOnClickListener {
-            tPicker.visibility = View.INVISIBLE
-            calView.visibility = View.VISIBLE
-        }
-
-        rdoTime.setOnClickListener {
-            tPicker.visibility = View.VISIBLE
-            calView.visibility = View.INVISIBLE
-        }
-
-        chrono.setOnClickListener {
-            chrono.base = SystemClock.elapsedRealtime()
-            chrono.start()
-            chrono.setTextColor(Color.RED)
-            rdoCal.visibility = View.VISIBLE
-            rdoTime.visibility = View.VISIBLE
-        }
-
-        tvYear.setOnLongClickListener {
-            chrono.stop()
-            chrono.setTextColor(Color.BLUE)
-
-            tvYear.text = Integer.toString(selectYear)
-            tvMonth.text = Integer.toString(selectMonth)
-            tvDay.text = Integer.toString(selectDay)
-
-            tvHour.text = Integer.toString(tPicker.hour)
-            tvMinute.text = Integer.toString(tPicker.minute)
-
-            tPicker.visibility = View.INVISIBLE
-            calView.visibility = View.INVISIBLE
-            rdoCal.visibility = View.INVISIBLE
-            rdoTime.visibility = View.INVISIBLE
-
-            false
-        }
-
-        calView.setOnDateChangedListener { _, year, month, day ->
-            selectYear = year
-            selectMonth = month
-            selectDay = day
-        }
+        var multi = findViewById<MultiAutoCompleteTextView>(R.id.multiAutoCompleteTextView1)
+        var token = MultiAutoCompleteTextView.CommaTokenizer()
+        multi.setTokenizer(token)
+        multi.setAdapter(adapter)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
