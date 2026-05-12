3bc203c491171c960aa1eb080a8fab8e604796bb
k0278kim <k0278kim@gmail.com>
2026-05-12 13:43:02 +0900

    11주차 7-2

--- app/src/main/java/com/example/assignment/MainActivity.kt
+++ app/src/main/java/com/example/assignment/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.assignment
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var baseLayout: LinearLayout
+    lateinit var button1: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,10 +24,55 @@ class MainActivity : AppCompatActivity() {
         title = "202235245 김태윤 8주차"
 
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        baseLayout = findViewById<LinearLayout>(R.id.baseLayout)
+        button1 = findViewById<Button>(R.id.button1)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.baseLayout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+
+//        menu!!.add(0, 1, 0, "배경색 (빨강)")
+//        menu!!.add(0, 2, 0, "배경색 (초록)")
+//        menu!!.add(0, 3, 0, "배경색 (파랑)")
+//
+//        var sMenu: SubMenu = menu.addSubMenu("버튼 변경 >> ")
+//        var
+
+        var mInflater = menuInflater
+        mInflater.inflate(R.menu.menu1, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.itemRed -> {
+                baseLayout.setBackgroundColor(Color.RED)
+                return true
+            }
+            R.id.itemBlue -> {
+                baseLayout.setBackgroundColor(Color.BLUE)
+                return true
+            }
+            R.id.itemGreen -> {
+                baseLayout.setBackgroundColor(Color.GREEN)
+                return true
+            }
+            R.id.subRotate -> {
+                button1.rotation = 45f
+                return true
+            }
+            R.id.subSize -> {
+                button1.scaleX = 2f
+                return true
+            }
+        }
+        return false
     }
 }
