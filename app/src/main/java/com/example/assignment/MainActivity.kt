f196e5030281173bfd367333f7f98214a4df5d09
k0278kim <k0278kim@gmail.com>
2026-05-31 14:53:45 +0900

    13주차 실습9-1

--- app/src/main/java/com/example/assignment/MainActivity.kt
+++ app/src/main/java/com/example/assignment/MainActivity.kt
@@ -1,24 +1,107 @@
 package com.example.assignment
 
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val LINE = 1
+        const val CIRCLE = 2
+        var curShape = LINE
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         supportActionBar?.setDisplayShowHomeEnabled(true)
         supportActionBar?.setIcon(R.drawable.ic_launcher)
-        title = "202235245 김태윤 8주차"
+        title = "202235245 김태윤 13주차"
 
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        val myView = MyGraphicView(this)
+        setContentView(myView)
+
+        ViewCompat.setOnApplyWindowInsetsListener(myView) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+    }
+
+    private class MyGraphicView(context: Context) : View(context) {
+        var startX = -1
+        var startY = -1
+        var stopX = -1
+        var stopY = -1
+
+        override fun onTouchEvent(event: MotionEvent?): Boolean {
+            when (event!!.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    startX = event.x.toInt()
+                    startY = event.y.toInt()
+                }
+
+                MotionEvent.ACTION_MOVE, MotionEvent.ACTION_UP -> {
+                    stopX = event.x.toInt()
+                    stopY = event.y.toInt()
+                    this.invalidate()
+                }
+            }
+            return true
+        }
+
+        override fun onDraw(canvas: Canvas) {
+            super.onDraw(canvas)
+            val paint = Paint()
+            paint.isAntiAlias = true
+            paint.strokeWidth = 5f
+            paint.style = Paint.Style.STROKE
+            paint.color = Color.RED
+
+            when (curShape) {
+                LINE -> canvas.drawLine(startX.toFloat(), startY.toFloat(),
+                    stopX.toFloat(), stopY.toFloat(), paint)
+                CIRCLE -> {
+                    var radius = Math.sqrt(Math.pow((stopX - startX).toDouble(), 2.0) + Math.pow((stopY - startY).toDouble(), 2.0))
+                    canvas.drawCircle(startX.toFloat(), startY.toFloat(), radius.toFloat(), paint)
+                }
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menu!!.add(0, 1, 0, "선 그리기")
+        menu!!.add(0, 2, 0, "원 그리기")
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            1 -> {
+                Toast.makeText(applicationContext, "hi", Toast.LENGTH_SHORT).show()
+                curShape = LINE
+                return true
+            }
+
+            2 -> {
+                curShape = CIRCLE
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
